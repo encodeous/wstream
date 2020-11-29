45d4089de15d4a696b963b4599bc41d96da21065
encodeous <encodeous7@gmail.com>
2020-11-29 11:28:35 -0500

    Test

--- wstreamlib/WsStartup.cs
+++ wstreamlib/WsStartup.cs
@@ -9,6 +9,7 @@ using Microsoft.AspNetCore.Builder;
 using Microsoft.AspNetCore.Hosting;
 using Microsoft.AspNetCore.Http;
 using Microsoft.AspNetCore.HttpOverrides;
+using Microsoft.AspNetCore.Internal;
 using Microsoft.Extensions.Configuration;
 using Microsoft.Extensions.DependencyInjection;
 
